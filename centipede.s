87818b417ab1a7e870effcbb666e6499898f0f1e
Shahmeer Athar <shahmeer.athar@gmail.com>
2021-03-28 20:13:22 -0400

    Added preamble

---
+++ centipede.s
@@ -0,0 +1,26 @@
+#####################################################################
+#
+# CSC258H Winter 2021 Assembly Final Project
+# University of Toronto, St. George
+#
+# Student: Muhammad Shahmeer Athar, 100622471
+#
+# Bitmap Display Configuration:
+# - Unit width in pixels: 8
+# - Unit height in pixels: 8
+# - Display width in pixels: 256
+# - Display height in pixels: 256
+# - Base Address for Display: 0x10008000 ($gp) 
+#
+# Which milestone is reached in this submission?
+# (See the project handout for descriptions of the milestones)
+# - Milestone 0
+#
+# Which approved additional features have been implemented?
+# (See the project handout for the list of additional features)
+# 1. None yet
+#
+# Any additional information that the TA needs to know:
+# - None yet
+#
+#####################################################################
